b351884ab9313d32a25c628e5d422c2f7d44158f
Kim Foukaris <kimfoukaris@outlook.com>
2018-06-24 08:59:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ const returnLastTwoDrivers = function (drivers) {
   return drivers.slice(Math.max(drivers.length - 2, 2));
 }
 
-const selectingDrivers = [returnFirstTwoDrivers();, returnLastTwoDrivers();];
+const selectingDrivers = [returnFirstTwoDrivers();, returnLastTwoDrivers();]

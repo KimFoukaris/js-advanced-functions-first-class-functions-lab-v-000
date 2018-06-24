dc6dc5ba0f76aa666dff27c5a3df58f717d717f8
Kim Foukaris <kimfoukaris@outlook.com>
2018-06-24 08:56:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const returnFirstTwoDrivers = function (drivers) {
   return drivers.slice(0, 2);
 }
+
+const returnLastTwoDrivers = function (drivers) {
+  return drivers.slice(Math.max(drivers.length - 2, 2));
+}
